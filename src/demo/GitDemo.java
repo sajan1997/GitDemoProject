fdb10f80ffb5de0f88523c7a9c2d3d3a2a21a85c
serveradminaccount <serveradminaccount@serveradminaccount-Latitude-5590>
2021-08-03 21:45:22 +0530

    Added class GitDemo

---
+++ src/demo/GitDemo.java
@@ -0,0 +1,17 @@
+package demo;
+
+public class GitDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 = 10;
+		int num2 = 50;
+		
+		if(num1 > num2) {
+			System.out.println("Num1("+num1+") is greater");
+		}else {
+			System.out.println("Num2("+num2+") is greater");
+		}
+	}
+
+}
