b924c8bd81b8329540dfd95fc78a1cfd613a37c1
serveradminaccount <serveradminaccount@serveradminaccount-Latitude-5590>
2021-08-04 10:12:00 +0530

    commiting changes in GitTest

--- src/demo/GitTest.java
+++ src/demo/GitTest.java
@@ -6,6 +6,12 @@ public class GitTest {
 		// TODO Auto-generated method stub
 		
 		System.out.println("Git Test Class");
+		String lunch = "Yes";
+		if(lunch.equals("Yes")) {
+			System.out.println("Its lunch break");
+		}else {
+			System.out.println("Work time");
+		}
 	}
 
 }
